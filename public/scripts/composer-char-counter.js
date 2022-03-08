e003333e34572e27c4c3ccccd5a3cad82ca8552d
Keila Kopvillem <kmkopvillem@gmail.com>
2022-03-08 12:22:08 -0500

    character counter implemented

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("textarea").on('input', function() {
+    let charCount = (this.value).length;
+    $('output')[0].innerText = 140 - charCount;
+    if ($('output')[0].innerText < 0) {
+      $('output')[0].className = 'counter-under'
+    } else if ($('output')[0].innerText >= 0) {
+      $('output')[0].className = 'counter'
+    }
+  });
+});
